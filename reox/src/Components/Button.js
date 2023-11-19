d7ae653ac8649f3de6527944e96626415c135570
Randy T <randythai360@gmail.com>
2023-11-19 17:09:00 -0600

    better buttons

--- reox/src/Components/Button.js
+++ reox/src/Components/Button.js
@@ -2,12 +2,15 @@ import '../App.css';
 function Button() {
     return (
     <>
+    <div className="buttonContainer">
         <button className="button">
             File
         </button>
         <button className="button">
             Settings
         </button>
+
+    </div>
     </>
     );
 }
