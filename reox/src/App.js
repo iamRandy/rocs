4f5060ad49aa3bd9f5b863c9677cc9f054afe4c0
Randy T <randythai360@gmail.com>
2023-11-19 16:59:38 -0600

    buttons

--- reox/src/App.js
+++ reox/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Hero from './Components/Hero';
+import Button from './Components/Button';
 
 function App() {
   return (
     <div className="App">
+         <Button />
          <Hero />
     </div>
   );
---
+++ reox/src/Components/Button.js
@@ -0,0 +1,15 @@
+import '../App.css';
+function Button() {
+    return (
+    <>
+        <button className="button">
+            File
+        </button>
+        <button className="button">
+            Settings
+        </button>
+    </>
+    );
+}
+
+export default Button;
