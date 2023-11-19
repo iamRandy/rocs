c86680a3fbbdf35e6da5e4a41c673f7f2715e931
Randy T <randythai360@gmail.com>
2023-11-19 17:29:11 -0600

    test'

--- reox/src/Components/Hero.js
+++ reox/src/Components/Hero.js
@@ -1,9 +1,15 @@
 import './Hero.css'
+import { useState } from 'react'
 
 function Hero() {
+    const [val, setVal] = useState("Hello world");
+    const change = event => {
+        setVal(event.target.value);
+    }
     return (
     <div class="body">
         <h1>Welcome to Reox</h1>
+        <input onChange={change} className="doc" value = {val}/>
     </div>
     );
 }
